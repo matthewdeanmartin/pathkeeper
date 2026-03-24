ba36eba755c838fc6155d5ab208c6065ff84b753
Matthew Martin <matthewdeanmartin@gmail.com>
2026-03-23 21:56:22 -0400

    0.1.0 more metadata

--- pathkeeper/__about__.py
+++ pathkeeper/__about__.py
@@ -1,9 +1,13 @@
 """Metadata for pathkeeper."""
 
 __all__ = [
+    "__credits__",
     "__description__",
+    "__keywords__",
+    "__license__",
     "__readme__",
     "__requires_python__",
+    "__status__",
     "__title__",
     "__version__",
 ]
@@ -12,4 +16,8 @@ __title__ = "pathkeeper"
 __version__ = "0.1.0"
 __description__ = "PATH backup, restore, and repair tool"
 __readme__ = "README.md"
-__requires_python__ = ">=3.13"
+__credits__ = [{"name": "Matthew Martin", "email": "matthewdeanmartin@gmail.com"}]
+__keywords__ = ["path"]
+__license__ = "MIT"
+__requires_python__ = ">=3.12"
+__status__ = "1 - Planning"
